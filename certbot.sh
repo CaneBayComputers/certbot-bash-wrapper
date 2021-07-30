c82ece7c6866c945fb8b0c4fe39362bdb2eaebef
Shawn Welch <101164+shrimpwagon@users.noreply.github.com>
2021-07-30 10:26:37 -0400

    Updates

--- certbot.sh
+++ certbot.sh
@@ -19,7 +19,6 @@ if $DEBUG; then DEBUG='-v --dry-run --test-cert'; else DEBUG=''; fi
 
 # https://certbot.eff.org/docs/using.html
 certbot -n $DEBUG --agree-tos --email $EMAIL --no-eff-email --manual-public-ip-logging-ok --manual \
-	--manual-auth-hook $BASEDIR/auth_hook.sh --manual-cleanup-hook $BASEDIR/cleanup_hook.sh \
-	--preferred-challenges dns --cert-name $NAME --expand -d $DOMAINS certonly
+	--manual-auth-hook $BASEDIR/auth_hook.sh --preferred-challenges dns --cert-name $NAME --expand -d $DOMAINS certonly
 
 if [ ! -z "$POST_CMD" ]; then $POST_CMD; fi
