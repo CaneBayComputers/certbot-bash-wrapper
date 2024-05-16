db1eca30527f0d4a66716027386ce7ea37428f2e
Shawn Welch <shrimpwagon@gmail.com>
2024-05-16 14:27:02 -0400

    Godaddy updates

--- auth_check_godaddy.sh
+++ auth_check_godaddy.sh
@@ -8,4 +8,6 @@ AUTH="Authorization: sso-key $GODADDY_KEY:$GODADDY_SECRET"
 
 echo $AUTH
 
-curl -X GET -H "$AUTH" "https://api.godaddy.com/v1/domains" > /dev/null 2>&1
+RESPONSE=$(curl -s -X GET -H "$AUTH" "https://api.godaddy.com/v1/domains")
+
+if echo $RESPONSE | grep "ACCESS_DENIED"; then exit 1; fi
